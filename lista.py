37e567456c3e1089b1f37d667a68bea0480ac059
andreatr09 <72097831+andreatr09@users.noreply.github.com>
2020-11-19 12:58:16 -0600

    Add files via upload

--- lista.py
+++ lista.py
@@ -7,5 +7,55 @@ class LinkedList:
     def __init__( self ):
         self.__head = None
 
-        def is_empty (self):
-            return self.__head == None
+    def is_empty (self):
+        return self.__head == None
+
+    def append( self, value ):
+        nuevo = Nodo (value)
+        if self.__head == None:  ## NOTE: self.is.empty()
+			self.__head = nuevo
+        else:
+            curr_node = self.__head
+            while curr_node.siguiente != None:
+                curr_node = curr_node.siguiente
+            curr_node.siguiente = nuevo
+
+    def transversal( self ):
+		curr_node = self.__head
+		while curr_node != None:
+			print( f" { curr_node.data } ->", end="" )
+			curr_node = curr_node.siguiente
+		print(" ")
+
+	def remove( self , value ):
+		curr_node = self.__head
+		if self.__head.data == values:
+			self.__head = self.__head.siguiente
+		else:
+			anterior = None
+			while curr_node.data != value and curr_node.siguiente != None:
+				anterior = curr_node
+				curr_node = curr_node.siguiente
+			if curr_node.data == value:
+				anterior.siguiente = curr_node.siguiente
+			else:
+				print("El dato no existe en la lista")
+
+	def preppend( self , value ):
+		nuevo = Nodo( value , self.__head )
+		self.__head = nuevo
+
+	def tail( self ):
+		curr_node = self.__head
+		while curr_node != None:
+			curr_node = curr_node.siguiente
+		return curr_node
+
+	def get( self , posicion=None ): ## NOTE: Por defecto regresa el ultimo
+		contador = 0
+		dato = None
+		if posicion == None:
+			dato = self.tail().data
+		else:
+			pass
+		return dato
---
+++ pruebaslistas.py
@@ -0,0 +1,15 @@
+from listas import LinkedList
+
+l=LinkedList()
+print (f"L essta vacia? {l.is_empty()})
+l.append(10)
+l.append(5)
+l.append(6)
+l.append(20)
+print( f"L esta vacia ? {l.is_empty()} )
+l.transversal()
+l.remove (10)
+l.transversal()
+x = l.tail()
+print( x.data )
+print(l.get())
