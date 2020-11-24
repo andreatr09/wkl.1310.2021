3b85379e06649d88e3fa2518e3970911b1150030
andreatr09 <72097831+andreatr09@users.noreply.github.com>
2020-11-24 15:12:37 -0600

    24-11-20

---
+++ pruebas_listadoble.py
@@ -0,0 +1,12 @@
+from listas import DoubleLinkedList
+
+ld = DoubleLinkedList()
+print( "Esta vacia?" , ld.is_empty() )
+ld.append( 10 )
+ld.append( 20 )
+ld.append( 30 )
+print( f" La lista tiene { ld.get_size() } elementos " )
+ld.transversal()
+ld.revese_transversal()
+ld.remove_from_head(20)
+ld.transversal()
