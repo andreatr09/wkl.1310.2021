5714a9bb6c155879d9e262cf5322691cb2a159b8
andreatr09 <72097831+andreatr09@users.noreply.github.com>
2021-01-21 12:52:08 -0600

    CLASE 21/01/21

--- arboles_binarios.py
+++ arboles_binarios.py
@@ -28,7 +28,7 @@ class BinarySerchTree:
 			else:
 				self.__insert__( nodo.left , value )
 		else:
-			if nodo.right == Nonne:
+			if nodo.right == None:
 				nodo.right = NodoArbol( value )
 			else:
 				self.__insert__( nodo.right , value )
@@ -39,12 +39,57 @@ class BinarySerchTree:
 			print( nodo.data , end = "," )
 			self.__recorrido__in( nodo.right )
 
-	def transversal( self , format="inorden" ):
+	def _recorrido_pos( self , nodo ):
+		   if nodo:
+			   self.__recorrido_pos(nodo.left)
+			   self.__recorrido_pos(nodo.right)
+			   print(nodo.data, end=", ")
+
+	def _recorrido_pre( self , nodo ):
+		   if nodo:
+			   self.__recorrido_pre(nodo.left)
+            	self.__recorrido_pre(nodo.right)
+            	print(nodo.data, end=", ")
+
+	def transversal( self, format = "inorden" ):
 		if format == "inorden":
 			self.__recorrido__in( self.__root )
 		elif format == "preorden":
 			print( "RECORRIDO EN PRE" )
+			self.__recorrido_pre( self.__root )
 		elif format == "posorden":
 			print( "POSORDEN" )
+			self.__recorrido_pos( self.__root )
 		else:
 			print( "ERROR , ESE FORMATO NO EXISTE" )
+
+	def search( self , value ):
+		if self.__root:
+			return None
+		else:
+			return self.__search( self.__root , value )
+
+	def __search( self , nodo , value ):
+		if nodo == None ## NOTE: VACIO ??? , CASO BASE
+			print("CASO BASE")
+			return None
+		elif nodo.data == value: # NOTE: COSO BASE DE RECURSIVIDAD
+			print("ENCONTRADO")
+			return nodo
+		elif value < nodo.data:
+			print("BUSCAR A LA IZQUIERDA")
+			return self.__search( nodo.left , value )
+		else:
+			print("BUSCAR A LA DERECHA")
+			return self.__search( nodo.right , value )
+
+	def remove( self , value ):
+		print( "ELIMINANDO" , encontrado.data )
+		encontrado = self.search( value )
+		# NOTE: CASO UNO --> ELIMINAR HOJA
+		if encontrado.left == None and encontrado.right == None:
+			encontrado = None
+		# NOTE: CASO DOS --> ELIMINAR PRADRE UN SOLO HIJO , SEA DERECHO O IZQUIERDO
+		elif (encontrado.left != None and encontrado.righ  == None) or \
+			(encontrado.left == None and encontrado.righ  != None):
+			print( "A ELIMINAR:" , encontrado.data )
--- pruebasAB.py
+++ pruebasAB.py
@@ -6,5 +6,16 @@ abb.insert(30)
 abb,insert(40)
 abb.insert(35)
 abb.insert(89)
+abb.insert(89)
 
+
+abb.transversal()
+
+abb.transversal("preorden")
+abb.transversal("posorden")
+
+res = abb.search(25)
+print("EL RESULTADO ES: {res}")
+
+abb.remove(35)
 abb.transversal()
